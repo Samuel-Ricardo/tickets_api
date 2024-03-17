d503bb2e8ea2940247d4a0744f6984abd1f0c034
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-17 20:42:02 -0300

    [ :sparkles: ] | create: model - TicketForCreate (app::model)

--- tickets_api/src/model/ticket.rs
+++ tickets_api/src/model/ticket.rs
@@ -1,8 +1,13 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize)]
 pub struct Ticket {
     pub id: u64,
     pub cid: u64, /* createor user_id  */
+    pub title: String,
+}
+
+#[derive(Deserialize)]
+pub struct TicketForCreate {
     pub title: String,
 }
