6601a1c118e928aee7d04153f78712390eb39498
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-17 19:31:02 -0300

    [ :sparkles: ] | setup: server (app::actix)

--- tickets_api/src/main.rs
+++ tickets_api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World! :D</strong>") }),
+    );
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("->> LISTENING on {:?}\n", listener.local_addr());
+
+    axum::serve(listener, routes_hello.into_make_service())
+        .await
+        .unwrap();
 }
