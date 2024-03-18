f30a9a4e1e13cfcf80e3d68ad64c769b1c51ff61
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-18 10:33:54 -0300

    [ :heavy_plus_sign: ] | add: dep - async_trait (async)

--- tickets_api/src/middleware/auth.rs
+++ tickets_api/src/middleware/auth.rs
@@ -1,4 +1,5 @@
-use anyhow::Ok;
+use axum::extract::{FromRequest, FromRequestParts};
+use axum::http::request::Parts;
 use axum::{body::Body, extract::State, http::Request, middleware::Next, response::Response};
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
@@ -17,7 +18,6 @@ pub async fn mw_require_auth(ctx: Result<CTX>, req: Request<Body>, next: Next) -
     Ok(next.run(req).await)
 }
 
-/*
 pub async fn mw_ctx_resolver(
     _mc: State<TicketController>,
     cookies: Cookies,
@@ -28,11 +28,28 @@ pub async fn mw_ctx_resolver(
 
     let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
-    let result_ctx = match auth_token.ok_or(Error::AuthFailNoAuthTokenCookie).and_then(parse_t) {
-
+    let result_ctx = match auth_token
+        .ok_or(Error::AuthFailNoAuthTokenCookie)
+        .and_then(parse_token)
+    {
+        Ok((user_id, _exp, _sign)) => Ok(CTX::new(user_id)),
+        Err(e) => Err(e),
     };
 }
-*/
+
+impl<S: Send + Sync> FromRequestParts<S> for CTX {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - CTX", "EXTRACTOR");
+
+        parts
+            .extensions
+            .get::<Result<CTX>>()
+            .ok_or(Error::AuthFailCtxNotInRequestExt)?
+            .clone();
+    }
+}
 
 fn parse_token(token: String) -> Result<(u64, String, String)> {
     let (_wholem, user_id, exp, sign) = regex_captures!(
