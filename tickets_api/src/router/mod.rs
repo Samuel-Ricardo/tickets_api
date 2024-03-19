a910a3b8fc613d23b1d3a36422cb6b34441fac52
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-19 09:55:02 -0300

    [ :vertical_traffic_light: ] | create: router - static (app::server)

--- tickets_api/src/router/mod.rs
+++ tickets_api/src/router/mod.rs
@@ -1,10 +1,18 @@
 pub mod login;
 pub mod tickets;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, get_service},
+    Router,
+};
+use tower_http::services::ServeDir;
 
 use crate::controller::hello_handler;
 
 pub fn hello_router() -> Router {
     Router::new().route("/", get(hello_handler))
+}
+
+pub fn statics() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
