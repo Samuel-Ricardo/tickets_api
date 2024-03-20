683aca281fd7647639fe66f2866bdd69e50c2d51
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-20 11:00:41 -0300

    [ :card_file_box: ] | create: sql - recreate-db (app::database)

---
+++ tickets_api/sql/dev_initial/00-recreate-db.sql
@@ -0,0 +1,15 @@
+-- DEV ONLY - Brute Force DROP DB (for local dev and unit test)
+SELECT
+  pg_terminate_backend(pid)
+FROM
+  pg_stat_activity
+WHERE
+  username = 'app_user'
+  OR datname = 'app_db';
+DROP DATABASE IF EXISTS app_db;
+DROP USER IF EXISTS app_user;
+
+-- DEV ONLY - Dev only password (for local dev and unit test).
+
+CREATE USER app_user PASSWORD 'dev_only_pws';
+CREATE DATABASE app_db owner app_user ENCODING = 'UTF-8';
