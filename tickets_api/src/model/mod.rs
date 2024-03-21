8c4fe8326087a3a4269f181d9e18f90056158b07
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-21 10:35:13 -0300

    [ :card_file_box: ] | setup: model - manager (app::model)

--- tickets_api/src/model/mod.rs
+++ tickets_api/src/model/mod.rs
@@ -1,4 +1,25 @@
+use self::store::{new_db_pool, DB};
+
 pub mod error;
 pub mod login;
 pub mod store;
+pub mod task;
 pub mod ticket;
+
+use error::Result;
+
+pub struct ModelManager {
+    db: DB,
+}
+
+impl ModelManager {
+    pub async fn new() -> Result<Self> {
+        let db = new_db_pool().await?;
+        Ok(ModelManager { db })
+    }
+
+    // INFO: Returns the SQLX DB Pool Reference [Only for the Model Layer]
+    pub(in crate::model) fn db(&self) -> &DB {
+        &self.db
+    }
+}
