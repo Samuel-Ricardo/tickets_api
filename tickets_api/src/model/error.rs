73e6799a61d3314f2f7e38fb4b851f6b32db4b71
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-21 10:59:37 -0300

    [ :goal_net: ] | catch: errors - sqlx (app::model)

--- tickets_api/src/model/error.rs
+++ tickets_api/src/model/error.rs
@@ -1,16 +1,25 @@
 use serde::Serialize;
+use serde_with::{serde_as, DisplayFromStr};
 
 use crate::model::store;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+#[serde_as]
 #[derive(Debug, Serialize)]
 pub enum Error {
     Store(store::Error),
+    Sqlx(#[serde_as(as = "DisplayFromStr")] sqlx::Error),
 }
 
 impl From<store::Error> for Error {
     fn from(err: store::Error) -> Self {
         Self::Store(err)
+    }
+}
+
+impl From<sqlx::Error> for Error {
+    fn from(err: sqlx::Error) -> Self {
+        Self::Sqlx(err)
     }
 }
