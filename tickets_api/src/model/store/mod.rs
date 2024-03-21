905c2c9cb6633dc30659c0df774e8cc8051d4d4b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-21 10:42:04 -0300

    [ :card_file_box: ] | create: model - store (app::database)

--- tickets_api/src/model/store/mod.rs
+++ tickets_api/src/model/store/mod.rs
@@ -1,1 +1,17 @@
-pub mod error;
+mod error;
+
+pub use self::error::{Error, Result};
+
+use crate::config;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+
+pub type DB = Pool<Postgres>;
+
+pub async fn new_db_pool() -> Result<DB> {
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&config().DB_URL)
+        .await
+        .map_err(|ex| Error::FailToCreatePool(ex.to_string()))
+}
