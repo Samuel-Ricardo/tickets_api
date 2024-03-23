07c4cd512ecf6d8436d060eaeb1600b6b363d0f3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-23 17:38:51 -0300

    [ :white_check_mark: ] | create: test - service > user [find] (app::service)

--- tickets_api/src/service/user.rs
+++ tickets_api/src/service/user.rs
@@ -41,3 +41,26 @@ impl UserService {
         Ok(user)
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::_dev_utils;
+    use serial_test::serial;
+
+    async fn test_first_ok_demo1() -> Result<()> {
+        let manager = _dev_utils::init_test_db().await;
+        let ctx = CTX::root_ctx();
+        let name = "demo1";
+
+        let user: User = UserService::first_by_username(&ctx, &manager, name)
+            .await
+            .unwrap()
+            .unwrap();
+        //            .context("Should have user")?;
+
+        assert_eq!(user.name, name);
+        Ok(())
+    }
+}
