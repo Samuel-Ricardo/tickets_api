4cb305771e2263e0efedcd4f2c57928251a136c3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-23 17:23:56 -0300

    [ :sparkles: ] | create: service - get > user (app::service)

--- tickets_api/src/service/user.rs
+++ tickets_api/src/service/user.rs
@@ -13,3 +13,12 @@ pub struct UserService;
 impl DbBmc for UserService {
     const TABLE: &'static str = "user";
 }
+
+impl UserService {
+    pub async fn get<E>(ctx: &CTX, manager: &ModelManager, id: i64) -> Result<E>
+    where
+        E: UserBy,
+    {
+        base::db::get::<Self, _>(ctx, manager, id).await
+    }
+}
