29fea0be5740e18f1fb22a794640cdef351055f1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-23 08:53:36 -0300

    [ :sparkles: ] | update: model - Ticket (app::model)

--- tickets_api/src/model/task.rs
+++ tickets_api/src/model/task.rs
@@ -1,21 +1,20 @@
 use serde::{Deserialize, Serialize};
+use sqlb::Fields;
 use sqlx::prelude::FromRow;
 
-use crate::model::Result;
-
-#[derive(Debug, Clone, FromRow, Serialize)]
+#[derive(Debug, Clone, FromRow, Serialize, Fields)]
 pub struct Task {
     pub id: i64,
     pub title: String,
 }
 
 /* -- DTO -- */
-#[derive(Deserialize)]
+#[derive(Deserialize, Fields)]
 pub struct TaskForCreate {
     pub title: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Fields)]
 pub struct TaskForUpdate {
     pub title: Option<String>,
 }
