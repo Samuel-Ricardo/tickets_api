e76a6cafb008c51d8114d1036c417e7b7805cbad
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-24 10:45:22 -0300

    [ :goal_net: ] | catch: error - crypt (app::crypt)

--- tickets_api/src/model/error.rs
+++ tickets_api/src/model/error.rs
@@ -1,16 +1,23 @@
 use serde::Serialize;
 use serde_with::{serde_as, DisplayFromStr};
 
-use crate::model::store;
+use crate::{crypt, model::store};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[serde_as]
 #[derive(Debug, Serialize)]
 pub enum Error {
+    Crypt(crypt::Error),
     Store(store::Error),
     Sqlx(#[serde_as(as = "DisplayFromStr")] sqlx::Error),
     EntityNotFound { entity: &'static str, id: i64 },
+}
+
+impl From<crypt::Error> for Error {
+    fn from(err: crypt::Error) -> Self {
+        Self::Crypt(err)
+    }
 }
 
 impl From<store::Error> for Error {
