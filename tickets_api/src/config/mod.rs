7d58d8dbf67164cf8a1dfa20d0d1e5e951e140a5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-24 09:40:11 -0300

    [ :gear: ] | update: config - app (app::config)

--- tickets_api/src/config/mod.rs
+++ tickets_api/src/config/mod.rs
@@ -1,5 +1,5 @@
 use crate::{error::Error, Result};
-use std::{env, sync::OnceLock};
+use std::{env, str::FromStr, sync::OnceLock, u8};
 
 pub fn config() -> &'static Config {
     //static is always global
@@ -16,6 +16,10 @@ pub fn config() -> &'static Config {
 pub struct Config {
     pub DB_URL: String,
     pub WEB_FOLDER: String,
+    // INFO: Crypt
+    pub PWD_KEY: Vec<u8>,
+    pub TOKEN_KEY: Vec<u8>,
+    pub TOKEN_DURATION_SEC: f64,
 }
 
 impl Config {
@@ -23,8 +27,21 @@ impl Config {
         Ok(Config {
             DB_URL: get_env("SERVICE_DB_URL")?,
             WEB_FOLDER: get_env("SERVICE_WEB_FOLDER")?,
+            PWD_KEY: get_env_b64u_as_u8s("SERVICE_PWD_KEY")?,
+            TOKEN_KEY: get_env_b64u_as_u8s("SERVICE_TOKEN_KEY")?,
+            TOKEN_DURATION_SEC: get_env_parse("SERVICE_TOKEN_DURATION_SEC")?,
         })
     }
+}
+
+fn get_env_parse<T: FromStr>(name: &'static str) -> Result<T> {
+    get_env(name)?
+        .parse::<T>()
+        .map_err(|_| Error::ConfigWrongFormat(name))
+}
+
+fn get_env_b64u_as_u8s(name: &'static str) -> Result<Vec<u8>> {
+    base64_url::decode(&get_env(name)?).map_err(|_| Error::ConfigWrongFormat(name))
 }
 
 fn get_env(name: &'static str) -> Result<String> {
