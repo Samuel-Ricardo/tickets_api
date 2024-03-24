3f24071e8e10d81b484959c87702c045232b6951
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-24 11:56:14 -0300

    [ :passport_control: ] | setup: auth - controler (app::server)

--- tickets_api/src/controller/login.rs
+++ tickets_api/src/controller/login.rs
@@ -4,9 +4,12 @@ use tower_cookies::{Cookie, Cookies};
 use tracing::debug;
 
 use crate::{
+    crypt::{pwd, EncryptContent},
+    ctx::CTX,
     error::{Error, Result},
     middleware::auth::AUTH_TOKEN,
-    model::{login::LoginPayload, ModelManager},
+    model::{login::LoginPayload, user::UserForLogin, ModelManager},
+    service::user::UserService,
 };
 
 pub async fn api_login_handler(
@@ -16,9 +19,31 @@ pub async fn api_login_handler(
 ) -> Result<Json<Value>> {
     debug!(" {:<12} - api_login", "HANDLER");
 
-    if payload.username != "samuel" || payload.pwd != "123" {
-        return Err(Error::LoginFail);
-    }
+    let payload = LoginPayload {
+        username: payload.username.to_string(),
+        pwd: payload.pwd.to_string(),
+    };
+    let root_ctx = CTX::root_ctx();
+
+    let user: UserForLogin = UserService::first_by_username(&root_ctx, &manager, &payload.username)
+        .await
+        .unwrap()
+        .ok_or(Error::LoginFailUsernameNotFound)?;
+
+    let user_id = user.id;
+
+    let Some(pwd) = user.pwd else {
+        return Err(Error::LoginFailUserHashNoPwd { user_id });
+    };
+
+    pwd::validate_pwd(
+        &EncryptContent {
+            salt: user.pwd_salt.to_string(),
+            content: pwd.clone(),
+        },
+        &pwd,
+    )
+    .map_err(|_| Error::LoginFailPwdNotMathing { user_id })?;
 
     let mut cookie = Cookie::new(AUTH_TOKEN, "user-1.exp.sign");
     cookie.set_http_only(true);
